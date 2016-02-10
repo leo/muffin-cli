d8db07a269ceb84737a7fa783fa53130eb5887a3
Leonard Lamprecht <mindrun@icloud.com>
2016-02-10 12:06:30 +0100

    Add prompts for DB

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ module.exports = generators.Base.extend({
   constructor: function () {
     generators.Base.apply(this, arguments)
     this.sourceRoot(__dirname + '/../kit')
+    this.option('install')
   },
   prompting: function () {
     var done = this.async()
@@ -20,12 +21,35 @@ module.exports = generators.Base.extend({
         type: 'input',
         name: 'author',
         message: 'Author'
+      },
+      {
+        type: 'input',
+        name: 'db_host',
+        message: 'DB host',
+        default: 'localhost'
+      },
+      {
+        type: 'input',
+        name: 'db_name',
+        message: 'DB name',
+        default: 'muffin'
+      },
+      {
+        type: 'input',
+        name: 'db_user',
+        message: 'DB user'
+      },
+      {
+        type: 'input',
+        name: 'db_password',
+        message: 'DB password'
       }
     ]
 
     this.prompt(prompts, function (answers) {
       this.fields = {
-        year: new Date().getFullYear()
+        year: new Date().getFullYear(),
+        secret: (Math.random() + 1).toString(36).substring(2)
       }
 
       Object.assign(this.fields, answers)
@@ -45,6 +69,10 @@ module.exports = generators.Base.extend({
         this.fs.copyTpl(source, this.destinationPath(file), details)
       }
     }.bind(this))
+
+    if (!this.options.install) {
+      return
+    }
 
     this.installDependencies({
       bower: false,
