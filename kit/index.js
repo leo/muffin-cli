863c40bd4c22d52122dac9db898f2ff4303ce043
Leo <leo@users.noreply.github.com>
2016-03-25 14:05:19 +0100

    Ability to set routing methods

--- kit/index.js
+++ kit/index.js
@@ -2,11 +2,11 @@ const app = require('muffin')
 const router = app.router
 
 router.get('/', function *(next) {
+  console.log('Visited the front page!')
   yield next
 })
 
-app.run(router, {
-  render: {
-    defaultLayout: 'default'
-  }
-})
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(2000, app.listening)
