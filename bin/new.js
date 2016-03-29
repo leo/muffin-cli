7b8954cc0b8aced9cc03011a21651bca7c799e08
Leo <leo@users.noreply.github.com>
2016-03-29 20:09:04 +0200

    Allow users to specify a custom destination

--- bin/new.js
+++ bin/new.js
@@ -5,12 +5,18 @@ const fs = require('fs-extra')
 const walk = require('walk')
 const colors = require('colors')
 const mkdirp = require('mkdirp')
+const program = require('commander')
 
+program.parse(process.argv)
+
+const directory = program.args[program.args.length - 1]
+const targetDir = directory ? path.resolve(process.cwd(), directory) : process.cwd()
 const kit = __dirname + '/../kit'
 
-if (path.basename(process.cwd()) == 'kit') {
+if (path.basename(targetDir) == 'kit') {
   console.log('You shouldn\'t run ' + 'init'.gray + ' in here.')
   console.log('Please run it somewhere outside of the project.')
+
   process.exit(0)
 }
 
@@ -18,8 +24,8 @@ const walker = walk.walk(kit)
 
 walker.on('file', function (root, fileStats, next) {
   const way = root + '/' + fileStats.name
-  const subPath = root.replace(template, '')
-  const folder = process.cwd() + subPath
+  const subPath = root.replace(kit, '')
+  const folder = targetDir + subPath
 
   fs.ensureDirSync(folder)
 
@@ -30,5 +36,5 @@ walker.on('file', function (root, fileStats, next) {
 })
 
 walker.on('end', function() {
-  console.log('Generated new site in ' + process.cwd().gray)
+  console.log('Generated new site in ' + targetDir.gray)
 })
