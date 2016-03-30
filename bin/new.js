af1f35f835c82df45ec040dab3b18076b363aa15
Leo <leo@users.noreply.github.com>
2016-03-30 13:48:03 +0200

    Let user enter the details

--- bin/new.js
+++ bin/new.js
@@ -5,6 +5,7 @@ const fs = require('fs-extra')
 const colors = require('colors')
 const mkdirp = require('mkdirp')
 const program = require('commander')
+const inquirer = require('inquirer')
 
 program.parse(process.argv)
 
@@ -19,42 +20,78 @@ if (targetDir == kit) {
   process.exit(0)
 }
 
-const walker = fs.walk(kit)
-var files = []
-
-walker.on('data', item => {
-  files.push(item.path)
-})
-
-walker.on('end', () => {
-  files.shift()
-
-  const replaceDots = [
-    'name',
-    'base'
-  ]
-
-  for (var file of files) {
-    var filePath = path.parse(file)
-
-    for (var property of replaceDots) {
-      filePath[property] = filePath[property].replace('_', '.')
+const prompts = [
+  {
+    name: 'handle',
+    message: 'Project name',
+    default: path.basename(targetDir),
+    filter (input) {
+      return input.replace(' ', '').toLowerCase()
     }
-
-    var dest = path.join(targetDir, path.relative(kit, path.format(filePath)))
-
-    try {
-      fs.ensureDirSync(path.dirname(dest))
-    } catch (err) {
-      return console.error(err)
-    }
-
-    try {
-      fs.copySync(file, dest)
-    } catch (err) {
-      return console.error(err)
-    }
+  },
+  {
+    name: 'author',
+    message: 'Author'
+  },
+  {
+    name: 'db_host',
+    message: 'DB host',
+    default: 'localhost'
+  },
+  {
+    name: 'db_name',
+    message: 'DB name',
+    default: 'muffin'
+  },
+  {
+    name: 'db_user',
+    message: 'DB user'
+  },
+  {
+    type: 'password',
+    name: 'db_password',
+    message: 'DB password'
   }
+]
 
-  console.log('Generated new site in ' + targetDir.gray)
+inquirer.prompt(prompts, answers => {
+  const walker = fs.walk(kit)
+  var files = []
+
+  walker.on('data', item => {
+    files.push(item.path)
+  })
+
+  walker.on('end', () => {
+    files.shift()
+
+    const replaceDots = [
+      'name',
+      'base'
+    ]
+
+    for (var file of files) {
+      var filePath = path.parse(file)
+
+      for (var property of replaceDots) {
+        filePath[property] = filePath[property].replace('_', '.')
+      }
+
+      var dest = path.join(targetDir, path.relative(kit, path.format(filePath)))
+
+      try {
+        fs.ensureDirSync(path.dirname(dest))
+      } catch (err) {
+        return console.error(err)
+      }
+
+      try {
+        fs.copySync(file, dest)
+      } catch (err) {
+        return console.error(err)
+      }
+    }
+
+    console.log('Generated new site in ' + targetDir.gray)
+  })
 })
