08d9cee97f5c61cf9d8c5d7c9f9d5577959c023a
Leo <leo@users.noreply.github.com>
2016-03-31 12:56:03 +0200

    Compress SCSS

--- template/build.js
+++ template/build.js
@@ -2,6 +2,8 @@ const compileSass = require('broccoli-sass')
 const mergeTrees = require('broccoli-merge-trees')
 
 const sassDir = 'assets/scss'
-const styles = compileSass([sassDir], 'main.scss', 'styles.css')
+const styles = compileSass([sassDir], 'main.scss', 'styles.css', {
+  outputStyle: 'compressed'
+})
 
 module.exports = mergeTrees([styles])
