e0cac11a15a1b9038b9baa0a7e88c1fd405aa19d
Leo <leo@users.noreply.github.com>
2016-03-31 11:35:31 +0200

    Use broccoli as asset pipeline

---
+++ template/build.js
@@ -0,0 +1,7 @@
+const compileSass = require('broccoli-sass')
+const mergeTrees = require('broccoli-merge-trees')
+
+const sassDir = 'assets/scss'
+const styles = compileSass([sassDir], 'main.scss', 'styles.css')
+
+module.exports = mergeTrees([styles, scripts])
