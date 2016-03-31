e14d1225ed2b0888d574e3ec1f9883b54945fc7d
Leo <leo@users.noreply.github.com>
2016-03-31 13:16:34 +0200

    Move static files over

--- template/build.js
+++ template/build.js
@@ -1,17 +1,26 @@
-const compileSass = require('broccoli-sass')
 const mergeTrees = require('broccoli-merge-trees')
+const compileSass = require('broccoli-sass')
 const esTranspiler = require('broccoli-babel-transpiler')
+const funnel = require('broccoli-funnel')
 
-const sourceDir = 'assets'
+const dirs = {
+  scss: 'assets/scss',
+  js: 'assets/js',
+  images: 'assets/images'
+}
 
-const scripts = esTranspiler(sourceDir, {
-  compact: true
-})
-
-const styles = compileSass([sourceDir], 'main.scss', 'styles.css', {
+const styles = compileSass([dirs.scss], 'main.scss', 'styles.css', {
   outputStyle: 'compressed'
 })
 
-module.exports = mergeTrees([styles, scripts], {
+const scripts = esTranspiler(dirs.js, {
+  compact: true
+})
+
+const images = new funnel(dirs.images, {
+  destDir: 'images'
+})
+
+module.exports = mergeTrees([styles, scripts, images], {
   overwrite: true
 })
