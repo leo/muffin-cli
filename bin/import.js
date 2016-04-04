6e396fc088aecbfbf36612d1ddbec68c8ce4a79d
Leo <leo@users.noreply.github.com>
2016-04-04 13:49:45 +0200

    Introducing: Data importer
    :metal:

---
+++ bin/import.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node --use_strict
+
+const Mongonaut = require('mongonaut')
+const program = require('commander')
+const chalk = require('chalk')
+const fs = require('fs-extra')
+const path = require('path')
+const utils = require('../lib/utils')
+const connection = require('../lib/db').rope
+const models = require('../lib/get').models
+
+program.parse(process.argv)
+const args = program.args
+
+if (!utils.isSite()) {
+  utils.log(chalk.red('No site in here! You need to be within a site\'s directory.'))
+  return
+}
+
+if (args.length === 0) {
+  utils.log('You need to specify a file: ' + chalk.grey('muffin import file.json'))
+  return
+}
+
+function insertData (files) {
+  files = files.map(file => {
+    return path.parse(file)
+  })
+
+  let mongonaut = new Mongonaut({
+    user: process.env.DB_USER || '',
+    pwd: process.env.DB_PASSWORD || '',
+    db: process.env.DB_NAME || 'muffin'
+  })
+
+  let imports = []
+
+  for (let file of files) {
+    mongonaut.set('collection', file.name)
+    imports.push(mongonaut.import(path.format(file)))
+  }
+
+  connection.close()
+
+  Promise.all(imports).then(() => {
+    utils.log(chalk.green('Successfully imported data!'))
+  }, reason => {
+    utils.log('Not able to insert sample data!', reason)
+  })
+}
+
+connection.on('open', () => {
+  // Firstly make sure that all files exist
+  connection.db.listCollections().toArray((err, names) => {
+    if (err) {
+      return utils.log(err)
+    }
+
+    const collections = names.map(details => {
+      return details.name
+    })
+
+    for (let file of args) {
+      let details = path.parse(path.resolve(file))
+
+      if (!utils.exists(path.format(details))) {
+        utils.log(`The file "${details.base}" doesn\'t exist`)
+        process.exit(1)
+      }
+
+      if (collections.indexOf(details.name) === -1) {
+        utils.log(`Collection "${details.name}" doesn\'t exist`)
+        process.exit(1)
+      }
+    }
+
+    insertData(args)
+  })
+})
