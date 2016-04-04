dbde5b4b007d0a3a96846bacbd40f04b42599ace
Leo <leo@users.noreply.github.com>
2016-04-04 16:38:22 +0200

    Emit events correctly

--- template/index.js
+++ template/index.js
@@ -1,3 +1,8 @@
-export function listening() {
+import EventEmitter from 'events'
+const emitter = new EventEmitter()
+
+emitter.on('listening', (host, port) => {
   // This code will be executed after muffin started running
-}
+})
+
+export default emitter
