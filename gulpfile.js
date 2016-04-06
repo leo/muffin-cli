892308d00c9f6d947c46cf38c1d5db9c97f07478
Leonard Lamprecht <leo@users.noreply.github.com>
2016-04-06 16:10:05 +0200

    Use gulp to transpile binaries

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const cache = require('gulp-cached')
+
+const paths = {
+  bin: 'bin/*'
+}
+
+gulp.task('compile-bin', function () {
+  return gulp.src(paths.bin)
+  .pipe(cache('bin'))
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(gulp.dest('build/bin'))
+})
+
+gulp.task('watch', function () {
+  gulp.watch(paths.bin, ['compile-bin'])
+})
+
+gulp.task('default', ['watch', 'compile-bin'])
