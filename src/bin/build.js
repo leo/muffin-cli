fd1fe5d21fe8b1a243bc078ec2e1460ac831f4f2
Leonard Lamprecht <leo@users.noreply.github.com>
2016-04-08 14:09:40 +0200

    Make file watching work

--- src/bin/build.js
+++ src/bin/build.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 import broccoli from 'broccoli'
-import ncp from 'ncp'
-import path from 'path'
 import chalk from 'chalk'
 import program from 'commander'
 import { log, isSite } from '../lib/utils'
+import Builder from '../lib/tasks/build'
 
 program
   .option('-w, --watch', 'Rebuild site if files change')
@@ -17,48 +16,4 @@ if (!isSite()) {
 }
 
 const tree = broccoli.loadBrocfile()
-const builder = new broccoli.Builder(tree)
-
-function startWatching () {
-  const Watcher = require('broccoli-sane-watcher')
-  const watch = new Watcher(builder)
-
-  process.on('SIGINT', () => {
-    watch.close()
-    process.exit(0)
-  })
-
-  watch.on('change', results => {
-    if (!results.filePath) return
-    console.log(results)
-  })
-
-  watch.on('error', err => log(err))
-}
-
-builder.build().then(results => {
-  const dir = typeof results === 'string' ? results : results.directory
-  const buildTime = results.totalTime
-
-  // Copy files from tmp folder to the destination directory
-  // And make sure to follow symlinks while doing so
-  ncp(dir, process.cwd() + '/dist', {dereference: true}, err => {
-    if (err) throw err
-
-    if (buildTime) {
-      // The original built time is in nanoseconds, so we need to convert it to milliseconds
-      log(chalk.green(`Finished building after ${Math.floor(buildTime / 1e6)}ms.`))
-    } else {
-      log(chalk.green('Finished building.'))
-    }
-
-    if (program.watch) {
-      return startWatching()
-    }
-
-    builder.cleanup().catch(err => log(err))
-  })
-
-}).catch(err => {
-  throw err
-})
+new Builder(tree, program.watch)
