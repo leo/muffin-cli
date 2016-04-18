72394f05ec96d0b15ccedf5959c8bd767c818a3e
Leonard Lamprecht <mindrun@icloud.com>
2016-04-18 13:23:06 +0200

    Remove them again

--- template/index.js
+++
@@ -1,8 +0,0 @@
-import EventEmitter from 'events'
-const emitter = new EventEmitter()
-
-emitter.on('listening', (host, port) => {
-  // This code will be executed after muffin started running
-})
-
-export default emitter
