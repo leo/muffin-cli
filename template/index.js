7866bacb844efe5421d6fda5ae5829fc8ad8614e
Leonard Lamprecht <mindrun@icloud.com>
2016-04-20 12:45:53 +0200

    Specify custom route

--- template/index.js
+++ template/index.js
@@ -1,1 +1,9 @@
-console.log('Test!')
+const app = require('muffin')
+const router = app.router
+
+router.get('/', function *(next) {
+  // console.log('Home visited!')
+  yield next
+})
+
+app.run(router)
