736c0d57c27f3aa36f418918680a60039def598b
Leonard Lamprecht <mindrun@icloud.com>
2016-04-20 10:39:59 +0200

    Run template index

--- bin/serve.js
+++ bin/serve.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 import bin from 'commander'
+import { log } from '../lib/utils'
+import { execFile } from 'child_process'
 
 bin
   .option('-w, --watch', 'Rebuild site if files change')
   .option('-p, --port <port>', 'The port on which your site will be available', parseInt)
   .parse(process.argv)
 
-console.log('test')
+execFile('node', ['index.js'], (error, stdout, stderr) => {
+  if (error) log(error)
+  console.log(stdout)
+})
--- template/index.js
+++ template/index.js
@@ -1,8 +1,1 @@
-import EventEmitter from 'events'
-const emitter = new EventEmitter()
-
-emitter.on('listening', (host, port) => {
-  // This code will be executed after muffin started running
-})
-
-export default emitter
+console.log('Test!')
