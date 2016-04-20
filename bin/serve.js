f203ac874e4f764d9dbd9e4194538b866f5b83ba
Leonard Lamprecht <mindrun@icloud.com>
2016-04-20 10:43:26 +0200

    Move over certain parts from server

--- bin/serve.js
+++ bin/serve.js
@@ -1,13 +1,30 @@
 #!/usr/bin/env node
 
 import bin from 'commander'
-import { log } from '../lib/utils'
-import { execFile } from 'child_process'
+import { log, isSite } from '../lib/utils'
+import { execFile, exec } from 'child_process'
 
 bin
   .option('-w, --watch', 'Rebuild site if files change')
   .option('-p, --port <port>', 'The port on which your site will be available', parseInt)
   .parse(process.argv)
+
+if (!isSite()) {
+  log(chalk.red('No site in here!'))
+  process.exit(1)
+}
+
+// Build before serving if "dist" directory doesn't exist
+if (bin.watch || !exists(process.cwd() + '/dist')) {
+  const builder = exec('muffin build -w')
+
+  builder.stdout.on('data', data => process.stdout.write(chalk.green(data)))
+  builder.stderr.on('data', data => console.error(data))
+
+  builder.on('error', err => {
+    throw err
+  })
+}
 
 execFile('node', ['index.js'], (error, stdout, stderr) => {
   if (error) log(error)
