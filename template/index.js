0866695a6ddf19eea34571f26160019b68971701
Leonard Lamprecht <mindrun@icloud.com>
2016-04-21 13:37:17 +0200

    Adjust boilerplate parts

--- template/index.js
+++ template/index.js
@@ -1,9 +1,11 @@
 const app = require('muffin')
+
+/*
 const router = app.router
 
 router.get('/', function *(next) {
   // console.log('Home visited!')
   yield next
-})
+})*/
 
-app.run(router)
+app.run()
