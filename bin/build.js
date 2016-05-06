e3cce88dd686e9f2b06dc69ed856162083b29da5
Leonard Lamprecht <mindrun@icloud.com>
2016-05-06 15:28:25 +0200

    Completely switch to args

--- bin/build.js
+++ bin/build.js
@@ -2,14 +2,14 @@
 
 import broccoli from 'broccoli'
 import chalk from 'chalk'
-import program from 'commander'
+import args from 'args'
 import { log, isSite } from '../lib/utils'
 import Builder from '../lib/tasks/build'
 import config from '../lib/config'
 
-program
-  .option('-w, --watch', 'Rebuild site if files change')
+args
+  .option('watch', 'Rebuild site if files change')
   .parse(process.argv)
 
 const tree = broccoli.loadBrocfile()
-new Builder(tree, program.watch)
+new Builder(tree, args.watch)
--- bin/import.js
+++ bin/import.js
@@ -7,12 +7,9 @@ import Insert from '../lib/tasks/insert'
 import config from '../lib/config'
 
 args.parse(process.argv)
-let which = program.args._
+let which = args.raw._
 
 which.shift()
-which.shift()
-
-console.log(which)
 
 if (which.length === 0) {
   log('You need to specify a file or directory: ' + chalk.grey('muffin import file.json'))
